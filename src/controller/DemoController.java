00f0f0a48ac31727e9d5008d2c89b40537239998
Yashan97 <s.d.y.anupama@gmail.com>
2025-12-13 12:49:20 +0530

    add sout

--- src/controller/DemoController.java
+++ src/controller/DemoController.java
@@ -12,6 +12,17 @@ public class DemoController {
     public TextField txtAddress;
 
     public void addBtn(ActionEvent actionEvent) {
+            String id = txtId.getText();
+            String name = txtName.getText();
+            String address = txtAddress.getText();
+            String email = txtEmail.getText();
+
+        System.out.println("id is - "+id);
+        System.out.println("name is - "+name);
+        System.out.println("email is - "+email);
+        System.out.println("address is  - "+address);
+
+
 
     }
 }
